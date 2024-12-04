b383e2a94a004d1a6579171788ab3c8ca2954d9b
Javid Gulmaliyev <cavidgulmaliyev491995@gmail.com>
2024-12-04 20:51:39 +0400

    About edits

---
+++ src/js/libraries/swiper/sliders/about.js
@@ -0,0 +1,31 @@
+import Swiper from "swiper";
+import { Autoplay } from "swiper/modules";
+
+/** @type {HTMLDivElement} */
+const aboutSlider = document.querySelector(".about-slider");
+
+if (aboutSlider) {
+  const swiper = new Swiper(aboutSlider, {
+    modules: [Autoplay,],
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      "992.1": {
+        spaceBetween: 30,
+        direction: "vertical",
+        speed: 0,
+      },
+      "1312.1": {
+        spaceBetween: 40,
+        direction: "vertical",
+        speed: 0,
+      },
+    },
+    spaceBetween: 30,
+    direction: "horizontal",
+    speed: 300,
+    rewind: true,
+  });
+}
--- src/js/libraries/swiper/swiper.js
+++ src/js/libraries/swiper/swiper.js
@@ -1,5 +1,6 @@
 import "./sliders/hero.js";
 import "./sliders/type.js";
+import "./sliders/about.js";
 import "./sliders/products.js";
 import "./sliders/reviews.js";
 import "./sliders/trust.js";
