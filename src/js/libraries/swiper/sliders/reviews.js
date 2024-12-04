5fc12ce989f8801e932d79547cd6fbabeb018f2c
Javid Gulmaliyev <cavidgulmaliyev491995@gmail.com>
2024-12-04 05:13:41 +0400

    Reviews edits

--- src/js/libraries/swiper/sliders/reviews.js
+++ src/js/libraries/swiper/sliders/reviews.js
@@ -7,8 +7,30 @@ const reviewsSlider = reviews?.querySelector(".reviews-slider");
 
 if (reviewsSlider) {
   const swiper = initSlider(reviews, reviewsSlider, {
-    slidesPerView: 5,
-    spaceBetween: 20,
+    breakpoints: {
+      "390.1": {
+        slidesPerView: 1.703081597,
+        spaceBetween: 15,
+      },
+      "650.1": {
+        slidesPerView: 2.75,
+        spaceBetween: 15,
+      },
+      "768.1": {
+        slidesPerView: 3.5,
+        spaceBetween: 20,
+      },
+      "992.1": {
+        slidesPerView: 4.25,
+        spaceBetween: 20,
+      },
+      "1280.1": {
+        slidesPerView: 5,
+        spaceBetween: 20,
+      },
+    },
+    slidesPerView: 1.15,
+    spaceBetween: 15,
     rewind: true,
   });
 }
